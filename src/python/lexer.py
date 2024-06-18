58ae1fd902662edd2c30b721aa28999c4772fe27
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-06-18 23:11:50 +0100

    Add lexer for the grammar

--- src/python/lexer.py
+++ src/python/lexer.py
@@ -2,10 +2,10 @@ from collections import namedtuple
 from enum import Enum, global_enum
 
 TokenType = Enum("TokenType", [
-    "INTEGER", "FLOAT", "STRING", "BOOL" "IDENTIFIER",
+    "INTEGER", "FLOAT", "STRING", "BOOL", "IDENTIFIER",
     "INTEGER_TYPE", "FLOAT_TYPE", "STRING_TYPE", "BOOL_TYPE",
     "EQUALS_EQUALS", "BANG_EQUALS", "LESS_THAN", "GREATER_THAN", "LESS_EQUALS", "GREATER_EQUALS",
-    "STAR", "SLASH", "PLUS", "MINUS", "BANG",
+    "EQUALS", "STAR", "SLASH", "PLUS", "MINUS", "BANG",
     "SEMICOLON", "COMMA",
     "LEFT_BRACKET", "RIGHT_BRACKET", "LEFT_CURLY", "RIGHT_CURLY",
     "IF", "ELSE", "WHILE", "RETURN",
@@ -16,25 +16,92 @@ Token = namedtuple("Token", ["typ", "lexeme"])
 # when importing the types use TokenType
 global_enum(TokenType)
 
+KEYWORDS = {
+    "true": BOOL,
+    "false": BOOL,
+    "int": INTEGER_TYPE,
+    "float": FLOAT_TYPE,
+    "str": STRING_TYPE,
+    "bool": BOOL_TYPE,
+    "if": IF,
+    "else": ELSE,
+    "while": WHILE,
+    "return": RETURN,
+}
+
+DIGRAPHS = {
+    "==": EQUALS_EQUALS,
+    "!=": BANG_EQUALS,
+    "<=": LESS_EQUALS,
+    ">=": GREATER_EQUALS,
+}
+MONOGRAPHS = {
+    "<": LESS_THAN,
+    ">": GREATER_THAN,
+    "=": EQUALS,
+    "*": STAR,
+    "/": SLASH,
+    "+": PLUS,
+    "-": MINUS,
+    "!": BANG,
+    ";": SEMICOLON,
+    ",": COMMA,
+    "(": LEFT_BRACKET,
+    ")": RIGHT_BRACKET,
+    "{": LEFT_CURLY,
+    "}": RIGHT_CURLY,
+}
 
 def tokenise(src):
     cur = 0
     tokens = []
     while cur < len(src):
         char = src[cur]
-        if char.isalpha() or char == '_':
+        print(char, cur)
+        if char.isspace():
+            cur += 1
+            continue
+        elif char.isalpha() or char == '_':
             # identifier or keyword (including bool or type)
-            pass
+            i = 0
+            while src[cur + i].isalnum() or src [cur + i] == '_':
+                i += 1
+            lexeme = src[cur:cur + i]
+            typ = KEYWORDS.get(lexeme) or IDENTIFIER
         elif char.isnumeric():
             # int or float
-            pass
+            i = 0
+            typ = INTEGER
+            while src[cur + i].isnumeric():
+                i += 1
+                if src[cur + i] == '.' and typ == INTEGER and src[cur + i + 1].isnumeric():
+                    typ = FLOAT
+                    i += 1
+            lexeme = src[cur:cur + i]
         elif char == '"':
             # string
+            i = 0
+            while src[1]:
+                pass
             pass
-        # all the easy tokens
         else:
+            # all the easy tokens
+            for monograph, token in MONOGRAPHS.items():
+                if char == monograph:
+                    lexeme = monograph
+                    typ = token
+                    break
+            for digraph, token in DIGRAPHS.items():
+                if char + src[cur + 1] == digraph:
+                    lexeme = digraph
+                    typ = token
+                    break
+
+        if not lexeme:
             # syntax error (unexpected char)
-            pass
+            assert False, "Tokenisation: Syntax Error"
+        tokens.append(Token(typ, lexeme))
+        cur += len(lexeme)
     return tokens
 
 
@@ -48,4 +115,3 @@ if __name__ == "__main__":
         src = f.read()
     tokens = tokenise(src)
     print(tokens)
-    print(IDENTIFIER.value)
