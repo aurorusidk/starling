7e19b689ba06d08c4bd8e0e81426c3553c4efc60
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-06-28 15:13:27 +0100

    Add type classes for the type checker

---
+++ src/python/type_defs.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from enum import Enum, Flag, auto
+
+
+BaseTypeKind = Enum("BaseTypeKind", [
+    "INT", "FLOAT", "FRAC", "STR", "BOOL",
+])
+
+
+class BaseTypeFlag(Flag):
+    INTEGER = auto()
+    FLOAT = auto()
+    RATIONAL = auto()
+    STRING = auto()
+    BOOLEAN = auto()
+    
+    NUMERIC = INTEGER | FLOAT | RATIONAL
+
+
+class Type:
+    # base class for all types
+    pass
+
+
+@dataclass
+class BaseType(Type):
+    kind: BaseTypeKind
+    flags: BaseTypeFlag
+    string: str
+
+
+@dataclass
+class ArrayType(Type):
+    elem_type: Type
+    length: int
+
