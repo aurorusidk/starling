40267d4e40c269a8baf73506735584cc0ef870d7
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-06-29 19:28:33 +0100

    Make builtin types a name mapping

--- src/python/builtin.py
+++ src/python/builtin.py
@@ -1,13 +1,13 @@
 from type_defs import BasicType, BasicTypeKind, BasicTypeFlag
 
 
-types = (
-        BasicType(BasicTypeKind.INT, BasicTypeFlag.INTEGER, "int"),
-        BasicType(BasicTypeKind.FLOAT, BasicTypeFlag.FLOAT, "float"),
-        BasicType(BasicTypeKind.FRAC, BasicTypeFlag.RATIONAL, "frac"),
-        BasicType(BasicTypeKind.STR, BasicTypeFlag.STRING, "str"),
-        BasicType(BasicTypeKind.BOOL, BasicTypeFlag.BOOLEAN, "bool"),
-)
+types = {
+        "int": BasicType(BasicTypeKind.INT, BasicTypeFlag.INTEGER, "int"),
+        "float": BasicType(BasicTypeKind.FLOAT, BasicTypeFlag.FLOAT, "float"),
+        "frac": BasicType(BasicTypeKind.FRAC, BasicTypeFlag.RATIONAL, "frac"),
+        "str": BasicType(BasicTypeKind.STR, BasicTypeFlag.STRING, "str"),
+        "bool": BasicType(BasicTypeKind.BOOL, BasicTypeFlag.BOOLEAN, "bool"),
+}
 
 # TODO: builtin name definitions (vars and funcs)
 
