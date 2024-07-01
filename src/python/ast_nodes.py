200b96ec310f14855053cfce6c3ee1d43dd44438
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-01 17:51:37 +0100

    Add AST nodes for structs

--- src/python/ast_nodes.py
+++ src/python/ast_nodes.py
@@ -157,4 +157,13 @@ class VariableDeclr(Declr):
     typ: Type | None
     value: Expr
 
+@dataclass
+class FieldDeclr(Declr):
+    name: Identifier
+    typ: Type
+
+@dataclass
+class StructDeclr(Declr):
+    name: Identifier
+    fields: list[FieldDeclr]
 
