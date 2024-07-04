adacf346c3b58762c1dd5f5ada460074f0219565
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-04 16:53:51 +0100

    Add testing base for parser

---
+++ test/test_parser.py
@@ -0,0 +1,23 @@
+import unittest
+from src.python.parser import parse, Parser
+import src.python.ast_nodes as ast
+
+
+class TestParser(unittest.TestCase):
+    # basic testing for each node
+    def test_valid_declr(self):
+        pass
+
+    def test_valid_stmt(self):
+        pass
+
+    def test_valid_expr(self):
+        pass
+
+    def test_binop_precs(self):
+        # make sure that operator precedences are respected
+        pass
+
+    # testing for errors in parsing
+    # ensure every error condition is tested for
+    
