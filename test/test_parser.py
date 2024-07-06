6cf1acb41bb53883efc54ff9bb5e0d28e2273e2a
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-06 19:35:13 +0100

    Add testing for valid declaration parsing

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,4 +1,5 @@
 import unittest
+from src.python import lexer
 from src.python.parser import parse, Parser
 import src.python.ast_nodes as ast
 
@@ -6,7 +7,49 @@ import src.python.ast_nodes as ast
 class TestParser(unittest.TestCase):
     # basic testing for each node
     def test_valid_declr(self):
-        pass
+        tests = {
+            "fn test(x int, y int) int {}": ast.FunctionDeclr(
+                ast.Identifier("test"),
+                ast.TypeName(ast.Identifier("int")),
+                [
+                    ast.Parameter(
+                        ast.Identifier("x"),
+                        ast.TypeName(ast.Identifier("int")),
+                    ),
+                    ast.Parameter(
+                        ast.Identifier("y"),
+                        ast.TypeName(ast.Identifier("int")),
+                    ),
+                ],
+                ast.Block([]),
+            ),
+
+            "struct test {x int, y int}": ast.StructDeclr(
+                ast.Identifier("test"),
+                [
+                    ast.FieldDeclr(
+                        ast.Identifier("x"),
+                        ast.TypeName(ast.Identifier("int")),
+                    ),
+                    ast.FieldDeclr(
+                        ast.Identifier("y"),
+                        ast.TypeName(ast.Identifier("int")),
+                    ),
+                ],
+            ),
+
+            "var test int = x;": ast.VariableDeclr(
+                ast.Identifier("test"),
+                ast.TypeName(ast.Identifier("int")),
+                ast.Identifier("x"),
+            ),
+        }
+
+        p = Parser(None)
+        for test, expected in tests.items():
+            p.cur = 0
+            p.tokens = lexer.tokenise(test)
+            self.assertEqual(p.parse_declaration(), expected)
 
     def test_valid_stmt(self):
         pass
