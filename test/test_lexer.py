f9dd254cc05fbc972949e2f7de40c4a47daf5886
Matt <123matt.hew654@gmail.com>
2024-07-09 11:08:19 +0100

    Add remaining lexer tests
    Keyword-like identifiers, graphs with no spaces, and ignoring whitespace. Also reformatting.

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,65 +1,79 @@
 import unittest
-from src.python.lexer import tokenise, Token, TokenType as T
+from src.python.lexer import *
 #run with: python -m unittest .\test\test_lexer.py
 
 class TestLexer(unittest.TestCase):
     #keyword testing
     def test_valid_kw(self):
         tests = {
-            "true": [Token(T.BOOL, "true")],
-            "false": [Token(T.BOOL, "false")],
-            "if": [Token(T.IF, "if")],
-            "else": [Token(T.ELSE, "else")],
-            "while": [Token(T.WHILE, "while")],
-            "return": [Token(T.RETURN, "return")],
-            "var": [Token(T.VAR, "var")],
-            "fn": [Token(T.FUNC, "fn")],
-            "struct": [Token(T.STRUCT, "struct")]
+            "true": [Token(BOOLEAN, "true")],
+            "false": [Token(BOOLEAN, "false")],
+            "if": [Token(IF, "if")],
+            "else": [Token(ELSE, "else")],
+            "while": [Token(WHILE, "while")],
+            "return": [Token(RETURN, "return")],
+            "var": [Token(VAR, "var")],
+            "fn": [Token(FUNC, "fn")],
+            "struct": [Token(STRUCT, "struct")]
         }
 
         for test, expected in tests.items():
             self.assertEqual(tokenise(test), expected)
 
+        for keyword in KEYWORDS:
+            self.assertIn(keyword, tests)
+
     def test_uppercase_kw(self):
-        tests ={
-            "True": [Token(T.IDENTIFIER, "True")],
-            "False": [Token(T.IDENTIFIER, "False")],
-            "IF": [Token(T.IDENTIFIER, "IF")],
-            "fN": [Token(T.IDENTIFIER, "fN")],
-            "strUct": [Token(T.IDENTIFIER, "strUct")]
+        tests = {
+            "True": [Token(IDENTIFIER, "True")],
+            "False": [Token(IDENTIFIER, "False")],
+            "IF": [Token(IDENTIFIER, "IF")],
+            "fN": [Token(IDENTIFIER, "fN")],
+            "strUct": [Token(IDENTIFIER, "strUct")]
         }
 
         for test, expected in tests.items():
             self.assertEqual(tokenise(test), expected)
 
     def test_identifier_kw(self):
-        pass
-
-    #graph testing
-    def test_valid_mg(self):
         tests = {
-            "<": [Token(T.LESS_THAN, "<")],
-            ">": [Token(T.GREATER_THAN, ">")],
-            "=": [Token(T.EQUALS, "=")],
-            "*": [Token(T.STAR, "*")],
-            "/": [Token(T.SLASH, "/")],
-            "+": [Token(T.PLUS, "+")],
-            "-": [Token(T.MINUS, "-")],
-            "!": [Token(T.BANG, "!")],
-            ";": [Token(T.SEMICOLON, ";")],
-            ":": [Token(T.COLON, ":")],
-            ",": [Token(T.COMMA, ",")],
-            ".": [Token(T.DOT, ".")],
-            "(": [Token(T.LEFT_BRACKET, "(")],
-            ")": [Token(T.RIGHT_BRACKET, ")")],
-            "{": [Token(T.LEFT_CURLY, "{")],
-            "}": [Token(T.RIGHT_CURLY, "}")],
-            "[": [Token(T.LEFT_SQUARE, "[")],
-            "]": [Token(T.RIGHT_SQUARE, "]")]
+            "structure": [Token(IDENTIFIER, "structure")],
+            "string": [Token(IDENTIFIER, "string")],
+            "func": [Token(IDENTIFIER, "func")],
+            "variable": [Token(IDENTIFIER, "variable")]
         }
 
         for test, expected in tests.items():
             self.assertEqual(tokenise(test), expected)
+
+    #graph testing
+    def test_valid_mg(self):
+        tests = {
+            "<": [Token(LESS_THAN, "<")],
+            ">": [Token(GREATER_THAN, ">")],
+            "=": [Token(EQUALS, "=")],
+            "*": [Token(STAR, "*")],
+            "/": [Token(SLASH, "/")],
+            "+": [Token(PLUS, "+")],
+            "-": [Token(MINUS, "-")],
+            "!": [Token(BANG, "!")],
+            ";": [Token(SEMICOLON, ";")],
+            ":": [Token(COLON, ":")],
+            ",": [Token(COMMA, ",")],
+            ".": [Token(DOT, ".")],
+            "(": [Token(LEFT_BRACKET, "(")],
+            ")": [Token(RIGHT_BRACKET, ")")],
+            "{": [Token(LEFT_CURLY, "{")],
+            "}": [Token(RIGHT_CURLY, "}")],
+            "[": [Token(LEFT_SQUARE, "[")],
+            "]": [Token(RIGHT_SQUARE, "]")]
+        }
+
+        for test, expected in tests.items():
+            self.assertEqual(tokenise(test), expected)
+
+        for monograph in MONOGRAPHS:
+            self.assertIn(monograph, tests)
 
     def test_invalid_mg(self):
         tests = [
@@ -70,39 +84,57 @@ class TestLexer(unittest.TestCase):
         ]
 
         for test in tests:
-            with self.assertRaises(AssertionError):
-                tokenise(test)
+            self.assertRaises(AssertionError, tokenise, test)
 
     def test_valid_dg(self):
         tests = {
-            "==": [Token(T.EQUALS_EQUALS, "==")],
-            "!=": [Token(T.BANG_EQUALS, "!=")],
-            "<=": [Token(T.LESS_EQUALS, "<=")],
-            ">=": [Token(T.GREATER_EQUALS, ">=")]
+            "==": [Token(EQUALS_EQUALS, "==")],
+            "!=": [Token(BANG_EQUALS, "!=")],
+            "<=": [Token(LESS_EQUALS, "<=")],
+            ">=": [Token(GREATER_EQUALS, ">=")]
         }
         
         for test, expected in tests.items():
             self.assertEqual(tokenise(test), expected)
 
+        for digraph in DIGRAPHS:
+            self.assertIn(digraph, tests)
+
     def test_invalid_dg(self):
         # no need to check for invalid chars
         # only checking sequences of valid chars that don't form digraphs
         tests = {
-            "<>": [Token(T.LESS_THAN, "<"), Token(T.GREATER_THAN, ">")],
-            "::": [Token(T.COLON, ":"), Token(T.COLON, ":")],
-            "++": [Token(T.PLUS, "+"), Token(T.PLUS, "+")],
-            "*=": [Token(T.STAR, "*"), Token(T.EQUALS, "=")]
+            "<>": [Token(LESS_THAN, "<"), Token(GREATER_THAN, ">")],
+            "::": [Token(COLON, ":"), Token(COLON, ":")],
+            "++": [Token(PLUS, "+"), Token(PLUS, "+")],
+            "*=": [Token(STAR, "*"), Token(EQUALS, "=")]
         }
 
         for test, expected in tests.items():
             self.assertEqual(tokenise(test), expected)
 
     def test_no_space_g(self):
-        pass
+        tests = {
+            "1+2": [Token(INTEGER, "1"), Token(PLUS, "+"), Token(INTEGER, "2")],
+            "x=9": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(INTEGER, "9")],
+            "test==true": [Token(IDENTIFIER, "test"), Token(EQUALS_EQUALS, "=="), Token(BOOLEAN, "true")]
+        }
+
+        for test, expected in tests.items():
+            self.assertEqual(tokenise(test), expected)
 
     #misc
     def test_ignore_whitespace(self):
-        pass
+        tests = {
+            "1       +  2      ": [Token(INTEGER, "1"), Token(PLUS, "+"), Token(INTEGER, "2")],
+            "    x   =9    ": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(INTEGER, "9")],
+            "   test     ==     true    ": [Token(IDENTIFIER, "test"), Token(EQUALS_EQUALS, "=="), Token(BOOLEAN, "true")],
+            "\t \t \t \t \t \t \t \t \t \t \t \t": [],
+            "\t       \t x\t \t =\t     \n        \t 25.7\t      \t": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(FLOAT, "25.7")]
+        }
+
+        for test, expected in tests.items():
+            self.assertEqual(tokenise(test), expected)
 
 if __name__ == "__main__":
     unittest.main()
