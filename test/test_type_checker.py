a21b9f820b8bbe0b2b816fa6e964bbb7f1ff526d
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-09 18:35:25 +0100

    Add placeholder for type checker testing

---
+++ test/test_type_checker.py
@@ -0,0 +1,18 @@
+import unittest
+from src.python.lexer import tokenise
+from src.python.parser import parse
+
+
+class TestTypeChecker(unittest.TestCase):
+    def test_valid_check(self):
+        pass
+
+    def test_invalid_check(self):
+        pass
+
+    def test_valid_inference(self):
+        pass
+
+    def test_invalid_inference(self):
+        pass
+
