7f9147024446a219f687f8753f4188d31b07dd80
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-15 11:56:45 +0100

    Refactor lexer tests to satisfy linter

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -1,76 +1,77 @@
 import unittest
-from src.python.lexer import *
+from src.python.lexer import Token, TokenType as T
+from src.python import lexer
 
 
 class TestLexer(unittest.TestCase):
     def test_valid_kw(self):
         tests = {
-            "true": [Token(BOOLEAN, "true")],
-            "false": [Token(BOOLEAN, "false")],
-            "if": [Token(IF, "if")],
-            "else": [Token(ELSE, "else")],
-            "while": [Token(WHILE, "while")],
-            "return": [Token(RETURN, "return")],
-            "var": [Token(VAR, "var")],
-            "fn": [Token(FUNC, "fn")],
-            "struct": [Token(STRUCT, "struct")],
+            "true": [Token(T.BOOLEAN, "true")],
+            "false": [Token(T.BOOLEAN, "false")],
+            "if": [Token(T.IF, "if")],
+            "else": [Token(T.ELSE, "else")],
+            "while": [Token(T.WHILE, "while")],
+            "return": [Token(T.RETURN, "return")],
+            "var": [Token(T.VAR, "var")],
+            "fn": [Token(T.FUNC, "fn")],
+            "struct": [Token(T.STRUCT, "struct")],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
-        for keyword in KEYWORDS:
+        for keyword in lexer.KEYWORDS:
             self.assertIn(keyword, tests)
 
     def test_uppercase_kw(self):
         tests = {
-            "True": [Token(IDENTIFIER, "True")],
-            "False": [Token(IDENTIFIER, "False")],
-            "IF": [Token(IDENTIFIER, "IF")],
-            "fN": [Token(IDENTIFIER, "fN")],
-            "strUct": [Token(IDENTIFIER, "strUct")],
+            "True": [Token(T.IDENTIFIER, "True")],
+            "False": [Token(T.IDENTIFIER, "False")],
+            "IF": [Token(T.IDENTIFIER, "IF")],
+            "fN": [Token(T.IDENTIFIER, "fN")],
+            "strUct": [Token(T.IDENTIFIER, "strUct")],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
     def test_identifier_kw(self):
         tests = {
-            "structure": [Token(IDENTIFIER, "structure")],
-            "string": [Token(IDENTIFIER, "string")],
-            "func": [Token(IDENTIFIER, "func")],
-            "variable": [Token(IDENTIFIER, "variable")],
+            "structure": [Token(T.IDENTIFIER, "structure")],
+            "string": [Token(T.IDENTIFIER, "string")],
+            "func": [Token(T.IDENTIFIER, "func")],
+            "variable": [Token(T.IDENTIFIER, "variable")],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
     def test_valid_mg(self):
         tests = {
-            "<": [Token(LESS_THAN, "<")],
-            ">": [Token(GREATER_THAN, ">")],
-            "=": [Token(EQUALS, "=")],
-            "*": [Token(STAR, "*")],
-            "/": [Token(SLASH, "/")],
-            "+": [Token(PLUS, "+")],
-            "-": [Token(MINUS, "-")],
-            "!": [Token(BANG, "!")],
-            ";": [Token(SEMICOLON, ";")],
-            ":": [Token(COLON, ":")],
-            ",": [Token(COMMA, ",")],
-            ".": [Token(DOT, ".")],
-            "(": [Token(LEFT_BRACKET, "(")],
-            ")": [Token(RIGHT_BRACKET, ")")],
-            "{": [Token(LEFT_CURLY, "{")],
-            "}": [Token(RIGHT_CURLY, "}")],
-            "[": [Token(LEFT_SQUARE, "[")],
-            "]": [Token(RIGHT_SQUARE, "]")],
+            "<": [Token(T.LESS_THAN, "<")],
+            ">": [Token(T.GREATER_THAN, ">")],
+            "=": [Token(T.EQUALS, "=")],
+            "*": [Token(T.STAR, "*")],
+            "/": [Token(T.SLASH, "/")],
+            "+": [Token(T.PLUS, "+")],
+            "-": [Token(T.MINUS, "-")],
+            "!": [Token(T.BANG, "!")],
+            ";": [Token(T.SEMICOLON, ";")],
+            ":": [Token(T.COLON, ":")],
+            ",": [Token(T.COMMA, ",")],
+            ".": [Token(T.DOT, ".")],
+            "(": [Token(T.LEFT_BRACKET, "(")],
+            ")": [Token(T.RIGHT_BRACKET, ")")],
+            "{": [Token(T.LEFT_CURLY, "{")],
+            "}": [Token(T.RIGHT_CURLY, "}")],
+            "[": [Token(T.LEFT_SQUARE, "[")],
+            "]": [Token(T.RIGHT_SQUARE, "]")],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
-        for monograph in MONOGRAPHS:
+        for monograph in lexer.MONOGRAPHS:
             self.assertIn(monograph, tests)
 
     def test_invalid_mg(self):
@@ -82,54 +83,83 @@ class TestLexer(unittest.TestCase):
         ]
 
         for test in tests:
-            self.assertRaises(AssertionError, tokenise, test)
+            self.assertRaises(AssertionError, lexer.tokenise, test)
 
     def test_valid_dg(self):
         tests = {
-            "==": [Token(EQUALS_EQUALS, "==")],
-            "!=": [Token(BANG_EQUALS, "!=")],
-            "<=": [Token(LESS_EQUALS, "<=")],
-            ">=": [Token(GREATER_EQUALS, ">=")],
+            "==": [Token(T.EQUALS_EQUALS, "==")],
+            "!=": [Token(T.BANG_EQUALS, "!=")],
+            "<=": [Token(T.LESS_EQUALS, "<=")],
+            ">=": [Token(T.GREATER_EQUALS, ">=")],
         }
-        
-        for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
 
-        for digraph in DIGRAPHS:
+        for test, expected in tests.items():
+            self.assertEqual(lexer.tokenise(test), expected)
+
+        for digraph in lexer.DIGRAPHS:
             self.assertIn(digraph, tests)
 
     def test_invalid_dg(self):
         tests = {
-            "<>": [Token(LESS_THAN, "<"), Token(GREATER_THAN, ">")],
-            "::": [Token(COLON, ":"), Token(COLON, ":")],
-            "++": [Token(PLUS, "+"), Token(PLUS, "+")],
-            "*=": [Token(STAR, "*"), Token(EQUALS, "=")],
+            "<>": [Token(T.LESS_THAN, "<"), Token(T.GREATER_THAN, ">")],
+            "::": [Token(T.COLON, ":"), Token(T.COLON, ":")],
+            "++": [Token(T.PLUS, "+"), Token(T.PLUS, "+")],
+            "*=": [Token(T.STAR, "*"), Token(T.EQUALS, "=")],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
     def test_no_space_g(self):
         tests = {
-            "1+2": [Token(INTEGER, "1"), Token(PLUS, "+"), Token(INTEGER, "2")],
-            "x=9": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(INTEGER, "9")],
-            "test==true": [Token(IDENTIFIER, "test"), Token(EQUALS_EQUALS, "=="), Token(BOOLEAN, "true")],
+            "1+2": [
+                Token(T.INTEGER, "1"),
+                Token(T.PLUS, "+"),
+                Token(T.INTEGER, "2")
+            ],
+            "x=9": [
+                Token(T.IDENTIFIER, "x"),
+                Token(T.EQUALS, "="),
+                Token(T.INTEGER, "9")
+            ],
+            "test==true": [
+                Token(T.IDENTIFIER, "test"),
+                Token(T.EQUALS_EQUALS, "=="),
+                Token(T.BOOLEAN, "true")
+            ],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
 
     def test_ignore_whitespace(self):
         tests = {
-            "1       +  2      ": [Token(INTEGER, "1"), Token(PLUS, "+"), Token(INTEGER, "2")],
-            "    x   =9    ": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(INTEGER, "9")],
-            "   test     ==     true    ": [Token(IDENTIFIER, "test"), Token(EQUALS_EQUALS, "=="), Token(BOOLEAN, "true")],
+            "1       +  2      ": [
+                Token(T.INTEGER, "1"),
+                Token(T.PLUS, "+"),
+                Token(T.INTEGER, "2")
+            ],
+            "    x   =9    ": [
+                Token(T.IDENTIFIER, "x"),
+                Token(T.EQUALS, "="),
+                Token(T.INTEGER, "9")
+            ],
+            "   test     ==     true    ": [
+                Token(T.IDENTIFIER, "test"),
+                Token(T.EQUALS_EQUALS, "=="),
+                Token(T.BOOLEAN, "true")
+            ],
             "\t \t \t \t \t \t \t \t \t \t \t \t": [],
-            "\t       \t x\t \t =\t     \n        \t 25.7\t      \t": [Token(IDENTIFIER, "x"), Token(EQUALS, "="), Token(FLOAT, "25.7")],
+            "\t       \t x\t \t =\t     \n        \t 25.7\t      \t": [
+                Token(T.IDENTIFIER, "x"),
+                Token(T.EQUALS, "="),
+                Token(T.FLOAT, "25.7")
+            ],
         }
 
         for test, expected in tests.items():
-            self.assertEqual(tokenise(test), expected)
+            self.assertEqual(lexer.tokenise(test), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
