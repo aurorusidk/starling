087eccc8e61f5e3f4bd0f12634571fd9188d4f39
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-07-15 12:02:19 +0100

    Refactor type checker tests to satisfy linter

--- test/test_type_checker.py
+++ test/test_type_checker.py
@@ -23,7 +23,7 @@ class TestTypeChecker(unittest.TestCase):
             "1 + 1//2": builtin.types["frac"],
             "1 + 1.5": builtin.types["float"],
             "\"a\" + \"b\"": builtin.types["str"],
-            "3 / 2": builtin.types["float"], # TODO: in tc should this be rational?
+            "3 / 2": builtin.types["float"],  # in tc should this be rational?
             "x + 1": builtin.types["int"],
             "x * 0.5": builtin.types["float"],
             "!true": builtin.types["bool"],
@@ -40,7 +40,6 @@ class TestTypeChecker(unittest.TestCase):
             tc.scope.name_map |= names
             tc.check(tc.root)
             self.assertEqual(tc.root.typ, expected)
-
 
     def test_invalid_expr_check(self):
         names = {
@@ -98,7 +97,6 @@ class TestTypeChecker(unittest.TestCase):
             tc.scope.name_map |= names
             tc.function = names["test"]
             tc.check(tc.root)
-
 
     def test_invalid_stmt_check(self):
         names = {
@@ -197,9 +195,10 @@ class TestTypeChecker(unittest.TestCase):
                     return "a"
                 }
             """: types.FunctionType(
-                builtin.types["str"],[]),
-        #TODO: add type inference between numeric types for a single var (i.e. like the first tests for invalid, but for int to float)
+                builtin.types["str"], []),
         }
+        # TODO: add type inference between numeric types for a single var
+        #        (i.e. like the first tests for invalid, but for int to float)
 
         for test, expected, in tests.items():
             tokens = tokenise(test)
@@ -211,20 +210,20 @@ class TestTypeChecker(unittest.TestCase):
     def test_invalid_inference(self):
         tests = [
             """fn test() {
-            var x; 
-            x = 1; 
+            var x;
+            x = 1;
             x="a";
             }
             """,
             """fn test() {
-            var x; 
-            x = true; 
+            var x;
+            x = true;
             x="true";
             }
             """,
             """fn test() {
-            var x; 
-            x = 1.5; 
+            var x;
+            x = 1.5;
             x= "1.5";
             }
             """,
