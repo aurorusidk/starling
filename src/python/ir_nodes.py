c7582e3c95b1facc84b0194b7177db5c7f12dde2
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-08-05 21:09:33 +0100

    Add a string representation of the IR

--- src/python/ir_nodes.py
+++ src/python/ir_nodes.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass, field
+from hashlib import sha1
 
 from . import type_defs as types
 from .scope import Scope
+
+
+def id_hash(obj):
+    return sha1(str(id(obj)).encode("UTF-8")).hexdigest()[:4]
 
 
 class Object:
@@ -16,7 +21,7 @@ class Value(Object):
 
 @dataclass
 class Constant(Object):
-    value: Value
+    value: object
     typ: types.Type
 
 
@@ -101,7 +106,6 @@ class DefFunc(Instruction):
 @dataclass
 class Unary(Instruction):
     op: str
-    lhs: Object
     rhs: Object
 
 
@@ -115,3 +119,54 @@ class Binary(Instruction):
 @dataclass
 class Program:
     declrs: list[Instruction]
+
+
+class IRPrinter:
+    def __init__(self):
+        self.blocks_seen = []
+
+    def to_string(self, ir):
+        match ir:
+            case Value(value, _):
+                return str(value)
+            case Constant(value, _):
+                return str(value)
+            case StructTypeRef():
+                return f"{ir.name}{{{', '.join(ir.fields)}}}"
+            case FunctionSignatureRef():
+                return f"{ir.name}({', '.join(ir.params)})"
+            case Ref(name):
+                return name
+            case Block():
+                if id_hash(ir) not in self.blocks_seen:
+                    block_hash = id_hash(ir)
+                    self.blocks_seen.append(block_hash)
+                    instrs = '\n'.join(' ' + self.to_string(i) for i in ir.instrs)
+                    if not instrs:
+                        instrs = " [empty]"
+                    return f"{block_hash}:\n{instrs}"
+                return ""
+            case Declare(ref):
+                return f"DECLARE {self.to_string(ref)}"
+            case Assign(target, value):
+                return f"ASSIGN {self.to_string(target)} <- {self.to_string(value)}"
+            case Return(value):
+                return f"RETURN {self.to_string(value)}"
+            case Branch(block):
+                return f"BRANCH {id_hash(block)}\n{self.to_string(block)}"
+            case CBranch(condition, t_block, f_block):
+                return (
+                    f"CBRANCH {self.to_string(condition)} "
+                    f"{id_hash(t_block)} {id_hash(f_block)}\n"
+                    f"{self.to_string(t_block)}\n{self.to_string(f_block)}"
+                )
+            case DefFunc(target, block):
+                return f"DEFINE {self.to_string(target)}\n{self.to_string(block)}"
+            case Unary(op, rhs):
+                return f"{op}{self.to_string(rhs)}"
+            case Binary(op, lhs, rhs):
+                return f"({self.to_string(lhs)} {op} {self.to_string(rhs)})"
+            case Program(declrs):
+                return '\n'.join(self.to_string(d) for d in declrs)
+            case _:
+                assert False, ir
