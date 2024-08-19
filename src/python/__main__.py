7b8de1cbf0645aa021a2cb02245667b19a316dcf
Wren <75564966+aurorusidk@users.noreply.github.com>
2024-08-19 18:00:25 +0100

    Add CLI arguments for logging verbosity

--- src/python/__main__.py
+++ src/python/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from . import cmd
 
@@ -21,12 +22,21 @@ cf_g = parser.add_argument_group("control flow diagram")
 cf_g.add_argument("--cf-show", action="store_true", help="display a control flow diagram")
 cf_g.add_argument("--cf-path", help="save a cf-diagram at the given path")
 
+parser.add_argument("-v", "--verbosity", action="count")
+
 parser.add_argument("--test", action="store_true", help="causes the IRPrinter to enter test mode")
 
 parser.add_argument("filename", help="the file to translate")
 
+logging_levels = (logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG)
+
 args = vars(parser.parse_args())
 filename = args.pop("filename")
+verbosity = args.pop("verbosity", 0)
+logging_level = logging_levels[verbosity]
+logging.basicConfig(format="%(levelname)s: %(message)s")
+logging.getLogger().setLevel(logging_level)
+
 if args.get("interpret"):
     cmd.exec_file(filename, **args)
 elif args.get("compile"):
